a429cd70079174ce8605b847db744f8b04af1b88
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-04-26 11:20:18 +0900

    userとpictureの紐付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_104044) do
+ActiveRecord::Schema.define(version: 2021_04_26_014701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2021_04_24_104044) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "image"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_pictures_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_04_24_104044) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "pictures", "users"
 end
